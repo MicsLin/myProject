460b901f95ff5b7a250990f4f01feacd5c8d8b1e
MicsLin <greenriverlin@gmail.com>
2023-12-08 10:45:56 +0800

    v6
    將作品集分離成一包js檔，未來要新增作品，只需更改這包js檔跟i18n的文字敘述即可。覆蓋過去，不必重新打包。

--- assets/Pen.f7ea89e2.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.a1fe0cc5.js";const e={};function n(_,c){return null}var a=r(e,[["render",n]]);export{a as default};
--- assets/User.8c0f8cfa.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.afafa0e1.js";const e={};function _(c,n){return null}var s=r(e,[["render",_]]);export{s as default};
